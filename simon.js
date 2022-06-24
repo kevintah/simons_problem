11e15024388980f0be75d7697336b533e164b8dd
kevintah <kevintah@yahoo.com>
2022-06-24 07:58:25 -0700

    1 file modified -- non optimized solution to simon's problem

--- simon.js
+++ simon.js
@@ -1,4 +1,7 @@
-
+// Non optimized solution to simon's problem
+// classical version of the problem ( quantum implementation coming soon)
+// It finds the secret
+// Optimization coming soon
 
 var n = 3;
 
@@ -13,4 +16,27 @@ var simon_pairs = {
                     "111"	: "010"
                   } 
 
-console.log(simon_pairs);
+
+var keys = Object.keys(simon_pairs);
+var values = Object.values(simon_pairs);
+var match_pair = [];
+
+for (let i = 0; i < values.length; i++) {
+  for (let k = i + 1; k < values.length; k++) {
+      if (values[i] == values[k]) {
+        match_pair.push(i);
+        match_pair.push(k);
+        
+      }
+  }
+}
+
+var secret = (eval(keys[match_pair[0]] ^ keys[match_pair[1]]));
+
+
+
+console.log(keys);
+console.log(values);
+console.log(match_pair);
+
+console.log("The secret is" + " " + secret);
